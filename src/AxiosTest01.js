afc64703015ad441d8cf02f24706ada8cf78d795
keemsane <keemsane@gmail.com>
2022-04-28 16:06:54 +0900

    react board db 데이터 받기 (테스트 완료)

--- src/AxiosTest01.js
+++ src/AxiosTest01.js
@@ -6,9 +6,10 @@ class AxiosTest extends React.Component {
     //   스프링부트로 생성한 로컬 서버에 접속 시 CORS 오류 발생
     //   CORS (Cross-Origin Resource Sharing) : http 헤더를 사용해 한 출처에서 실행중인 웹 애플리케이션에서 다른 출처의 자원에 접근할 경우 오류 발생. 다른 서버와 데이터 연동하려면 이런 오류가 남.
     axios
-      .get('http://localhost:9090/ajax/test?data=test')
-    //   .get('/ajax/test?data=test')
-    //   .get('https://yts-proxy.now.sh/list_movies.json')
+      //   .get('/ajax/test?data=test')
+      //   .get('http://localhost:9090/ajax/test?data=test')
+      //   .get('https://yts-proxy.now.sh/list_movies.json')
+      .get('/ajax/boardList')
       .then((res) => {
         console.log('CONNECTION SUCCESS !!');
         console.log(res);
