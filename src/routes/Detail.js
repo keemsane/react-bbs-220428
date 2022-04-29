df79ff843cbdb253c82222a2b8df1a75da010ab5
keemsane <keemsane@gmail.com>
2022-04-29 16:32:58 +0900

    리액트 디테일, 글 작성 최종직전

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class Detail extends React.Component {
+  state = {
+    boardNo: '',
+    title: '',
+    content: '',
+    userId: '',
+    createDate: '',
+    hitCnt: 0,
+  };
+  componentDidMount() {
+    const boardNo = this.props.match.params.boardno;
+    const url = `ajax/boardDetail/${boardNo}`;
+    axios
+      .get(url)
+      .then((res) => {
+        console.log('CONNECTION SUCCESS !!');
+        console.log(res.data);
+        this.setState({
+          boardNo: res.data.boardNo,
+          title: res.data.title,
+          content: res.data.content,
+          userId: res.data.createId,
+          createDate: res.data.createDate,
+          hitCnt: res.data.hitCnt,
+        })
+      })
+      .catch((err) => {
+        console.log('CONNECTION FAILURE !!');
+        console.log(err);
+      });
+  }
   render() {
+    const data = this.props.match.params.boardno;
+    console.log(data);
     return (
       <div>
         <header class="container">
@@ -17,12 +51,7 @@ class Detail extends React.Component {
             <article>
               <div class="row my-3">
                 <div class="col-sm-12">
-                  <input
-                      type="text"
-                      class="form-control"
-                      id="title"
-                      placeholder="제목"
-                    ></input>
+                  <input type="text" class="form-control" id="title" placeholder="제목" value = {this.state.title}></input>
                 </div>
               </div>
               <div class="row">
@@ -32,10 +61,11 @@ class Detail extends React.Component {
                     class="form-control"
                     id="create-id"
                     placeholder="아이디"
+                    value={this.state.userId}
                   ></input>
                 </div>
                 <div class="col-sm-4">
-                  <input type="text" class="form-control" id="hit-cnt" placeholder="조회수"></input>
+                  <input type="text" class="form-control" id="hit-cnt" placeholder="조회수" value = {this.state.hitCnt}></input>
                 </div>
                 <div class="col-sm-4">
                   <input
@@ -43,25 +73,26 @@ class Detail extends React.Component {
                     class="form-control"
                     id="create-date"
                     placeholder="작성일"
+                    value={this.state.createDate}
                   ></input>
                 </div>
               </div>
               <div class="row my-3">
                 <div class="col-sm-12">
                   <div class="my-3">
-                    <textarea rows="5" class="form-control" id="content"></textarea>
+                    <textarea rows="5" class="form-control" id="content" value = {this.state.content}></textarea>
                   </div>
                 </div>
               </div>
               <div class="my-3 d-flex justify-content-between">
-                    <div>
-                      <button class="btn btn-secondary"> 목록 </button>
-                    </div>
-                    <div>                      
-                      <button class="btn btn-primary mx-3"> 등록 </button>
-                      <button class="btn btn-danger"> 삭제 </button>
-                    </div>
-                  </div>
+                <div>
+                  <button type = "button" class = "btn btn-secondary"> <Link style = {{color: 'inherit', textDecoration: 'inherit'}} to = "/">목록</Link> </button>
+                </div>
+                <div>
+                  <button class="btn btn-info mx-3"> 수정 </button>
+                  <button class="btn btn-danger"> 삭제 </button>
+                </div>
+              </div>
             </article>
           </section>
         </main>
--- src/routes/Write.js
+++ src/routes/Write.js
@@ -1,59 +1,104 @@
 import React from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class Write extends React.Component {
+  state = {
+    title: '',
+    content: '',
+    userId: '',
+  };
+
+  titleChange = (e) => {
+    this.setState({ title: e.target.value });
+  };
+
+  userIdChange = (e) => {
+    this.setState({ userId: e.target.value });
+  };
+
+  contentChange = (e) => {
+    this.setState({ content: e.target.value });
+  };
+
+  sendData = () => {
+    const datas = {
+      title: this.state.title,
+      content: this.state.content,
+      createId: this.state.userId,
+    };
+    axios
+      .post('/ajax/boardWrite', null, { params: datas } )
+      .then((res) => {
+        console.log('CONNECTION SUCCESS !!');
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log('CONNECTION FAILURE !!');
+        console.log(err);
+      });
+  };
   render() {
     return (
       <div>
-        <header class="container">
-          <div class="p-5 mb-4 bg-light rounded-3 text-center">
-            <div class="container-fluid py-5">
-              <h1 class="display-5 fw-bold">React와 SpringBoot를 활용한 게시판</h1>
+        <header className="container">
+          <div className="p-5 mb-4 bg-light rounded-3 text-center">
+            <div className="container-fluid py-5">
+              <h1 className="display-5 fw-bold">React와 SpringBoot를 활용한 게시판</h1>
               <p> 목록 보기 </p>
             </div>
           </div>
         </header>
-        <main class="container">
+        <main className="container">
           <section>
             <article>
-              <div class="row my-3">
-                <div class="col-sm-6 mx-auto">
-                  <div class="my-3">
-                    <label for="title"> 제목 : </label>
+              <div className="row my-3">
+                <div className="col-sm-6 mx-auto">
+                  <div className="my-3">
+                    <label htmlFor="title"> 제목 : </label>
                     <input
                       type="text"
-                      class="form-control"
+                      className="form-control"
                       id="title"
                       name="title"
                       placeholder="제목을 입력하세요"
+                      value={this.state.title}
+                      onChange={this.titleChange}
                     ></input>
                   </div>
-                  <div class="my-3">
-                    <label for="user-id">ID : </label>
+                  <div className="my-3">
+                    <label htmlFor="user-id">ID : </label>
                     <input
                       type="text"
-                      class="form-control"
+                      className="form-control"
                       id="user-id"
                       name="user-id"
                       placeholder="아이디를 입력하세요"
+                      value={this.state.userId}
+                      onChange={this.userIdChange}
                     ></input>
                   </div>
-                  <div class="my-3">
-                    <label for="content"> 글 내용 : </label>
+                  <div className="my-3">
+                    <label htmlFor="content"> 글 내용 : </label>
                     <textarea
                       rows="10"
-                      class="form-control"
+                      className="form-control"
                       id="content"
                       name="content"
                       placeholder="내용을 입력하세요"
+                      value={this.state.content}
+                      onChange={this.contentChange}
                     ></textarea>
                   </div>
-                  <div class="my-3 d-flex justify-content-between">
+                  <div className="my-3 d-flex justify-content-between">
                     <div>
-                      <button class="btn btn-secondary"> 목록 </button>
+                      <button type = "button" className="btn btn-secondary"><Link style={{ color: 'inherit', textDecoration: 'inherit'}} to = "/"> 목록 </Link></button>
                     </div>
-                    <div>                      
-                      <button class="btn btn-info mx-3"> 등록 </button>
-                      <button class="btn btn-warning"> 취소 </button>
+                    <div>
+                      <button type = "button" className="btn btn-info mx-3" onClick={this.sendData}>
+                        등록
+                      </button>
+                      <button type = "reset" className="btn btn-warning">취소</button>
                     </div>
                   </div>
                 </div>
@@ -61,8 +106,8 @@ class Write extends React.Component {
             </article>
           </section>
         </main>
-        <footer class="container-fluid mt-5 py-5 border-top">
-          <p class="lead text-mute text-center">made by BITC</p>
+        <footer className="container-fluid mt-5 py-5 border-top">
+          <p className="lead text-mute text-center">made by BITC</p>
         </footer>
       </div>
     );
